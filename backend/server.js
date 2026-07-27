439b0bda32ff7ac02afd18072fafa34b2ab5b673
Aman Kumar <mr.aman.kr2025@gmail.com>
2026-07-27 17:48:25 +0530

    initial mern setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require('express');
+const cors = require('cors');
+const DB = require("./config/db");
+
+DB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/" , (req , res)=>{
+    res.send("server is running");
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , ()=> {
+    console.log("server started");
+})
